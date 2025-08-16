a552ec26f96808430a06bc787a018f18602d2578
Jithendra-maremanda <jithendramaremanda@gmail.com>
2025-08-16 11:31:53 +0530

    code updated

--- 60-catalogue/variable.tf
+++ 60-catalogue/variable.tf
@@ -5,3 +5,15 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+variable "zone_id" {
+    description = "The ID of the Route 53 hosted zone"
+    type        = string
+    default = "Z07875261XFWESB6HKZDD"
+  
+}
+
+variable "zone_name" {
+    type        = string
+    default = "stansrobotshop.site"
+  
+}
